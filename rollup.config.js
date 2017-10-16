b47ecbdd66f1ac36aef3a8ad459d3a151e43dd7a
Christopher Carson <chris@nowzoo.com>
2017-10-16 12:18:05 -0500

    starter lib

--- rollup.config.js
+++ rollup.config.js
@@ -10,11 +10,11 @@ const globals = {
 };
 
 export default {
-    entry: './dist/modules/angular-library-starter.es5.js',
-    dest: './dist/bundles/angular-library-starter.umd.js',
+    entry: './dist/modules/simple-firebase-auth.es5.js',
+    dest: './dist/bundles/simple-firebase-auth.umd.js',
     format: 'umd',
     exports: 'named',
-    moduleName: 'ng.angularLibraryStarter',
+    moduleName: 'ng.simpleFirebaseAuth',
     plugins: [resolve()],
     external: Object.keys(globals),
     globals: globals,
