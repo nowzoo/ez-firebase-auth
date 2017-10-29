134fc872fd51eb6800da6670ffbe6758e5c33105
Christopher Carson <chris@nowzoo.com>
2017-10-29 14:17:46 -0500

    before separating out the demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { EzfaModule } from '../ezfa';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EzfaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
