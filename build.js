d8fa131d0851b592402f337f32eb3749cf28d268
Christopher Carson <chris@nowzoo.com>
2017-10-29 13:31:01 -0500

    finally published

--- build.js
+++ build.js
@@ -1,16 +1,17 @@
-const fs = require('fs-extra');
+
 const _ = require('lodash');
+const keysFromBuilt = ["main", "module", "es2015", "typings", "metadata"];
+const keysToOmitFromMain = ['devDependencies', 'dependencies', 'scripts', 'engines', 'filename']
+const fs = require('fs-extra');
 fs.readJson('./package.json', (err, result) => {
-  const dist = _.omit(result, 'devDependencies', 'dependencies', 'scripts', 'engines', 'filename');
-  const s = {
-    "main": "bundles/ez-firebase-auth.umd.js",
-  	"module": "@nowzoo/ez-firebase-auth.es5.js",
-  	"es2015": "@nowzoo/ez-firebase-auth.js",
-  	"typings": "ez-firebase-auth.d.ts",
-  	"metadata": "ez-firebase-auth.metadata.json"
-  }
-  const pkg = _.assign({}, dist, s);
-  fs.outputJson('./dist/package.json', pkg, {spaces: '\t'})
+  let main = _.omit(result, keysToOmitFromMain);
+  fs.readJson('./dist/package.json', (err, result) => {
+    let dist = result;
+    _.each(keysFromBuilt, key => {
+      main[key] = dist[key];
+    })
+    fs.outputJson('./dist/package.json', main, {spaces: '\t'})
+  });
 });
 
 fs.copy('./LICENSE', './dist/LICENSE');
